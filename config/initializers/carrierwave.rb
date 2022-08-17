e8d79f9110c935c6c5a665d6edf641602e81e80f
Fernando Mendes <fernando.msantos1@gmail.com>
2022-08-17 20:30:44 -0300

    Show image from local or remote sftp upload

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
   config.root = '/opt/storage_site'
   config.cache_storage = :file
-  config.sftp_host = '192.168.15.5'
+  config.sftp_host = '192.168.15.6'
   config.sftp_user = 'vagrant'
   config.sftp_folder = '/opt/storage_site'
   #config.sftp_url = "192.168.15.3"
